8ddbb5fb4f4d4458c7ec55eedab141fd2cbc205e
Fernando Araujo <fernandovcaraujo@gmail.com>
2020-09-25 14:00:20 -0300

    Entidade Usuario criada + BuscarPorId

---
+++ Edux/Interfaces/IUsuarioRepository.cs
@@ -0,0 +1,48 @@
+﻿using Edux.Domains;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Edux.Interfaces
+{
+    interface IUsuarioRepository
+    {
+        /// <summary>
+        /// Lista todos os usuarios na plataforma
+        /// </summary>
+        /// <returns>retorna uma lista de usuarios</returns>
+        List<Usuario> Listar();
+
+        Usuario BuscarPorId(Guid id);
+
+        /// <summary>
+        /// Adiciona novos usuario
+        /// </summary>
+        /// <param name="usuario">Usuario</param>
+        void Adicionar(Usuario usuario);
+
+        /// <summary>
+        /// filtro por nome
+        /// </summary>
+        /// <param name="nome"></param>
+        /// <returns></returns>
+        List<Usuario> BuscarPorNome(string nome);
+
+        /// <summary>
+        /// altera o usuario existente
+        /// </summary>
+        /// <param name="usuario">Usuario</param>
+        void Editar(Usuario usuario);
+
+        /// <summary>
+        /// Deleta o usuario pelo id
+        /// </summary>
+        /// <param name="id">id do usuario</param>
+        void Remover(Guid id);
+
+        //DataUltimoAcesso???
+
+
+    }
+}
---
+++ Edux/Repositories/UsuarioRepository.cs
@@ -0,0 +1,52 @@
+﻿using Edux.Contexts;
+using Edux.Domains;
+using Edux.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Edux.Repositories
+{
+    public class UsuarioRepository : IUsuarioRepository
+    {
+        private readonly EduxContext _context = new EduxContext();
+
+        public void Adicionar(Usuario usuario)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Usuario BuscarPorId(Guid id)
+        {
+            try
+            {
+                return _context.Usuario.Find(id);
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
+
+        public List<Usuario> BuscarPorNome(string nome)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Editar(Usuario usuario)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Usuario> Listar()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Remover(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
